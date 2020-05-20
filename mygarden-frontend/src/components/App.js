c74d111c07b8cac17083cf0855cfdafc637be175
oliverfeher <oliver92.feher@gmail.com>
2020-05-20 16:29:37 -0400

    Reinit fronend

---
+++ mygarden-frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../stylesheets/App.css";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import Home from './Home';
+import Login from './Login';
+import Logout from './Logout';
+
+
+const App = () =>
+{
+  return <div>Testt</div>
+}
+
+
+export default App;
