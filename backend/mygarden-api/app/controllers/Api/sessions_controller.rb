75f9c46434efb92c8101449c2d0fa1499f98d6d7
oliverfeher <oliver92.feher@gmail.com>
2020-05-21 07:53:59 -0400

    Build conditinal for user login in
    - If pwd correct create and encode a JTW token send as JSON
    - Else send error

--- backend/mygarden-api/app/controllers/Api/sessions_controller.rb
+++ backend/mygarden-api/app/controllers/Api/sessions_controller.rb
@@ -1,14 +1,30 @@
 class Api::SessionsController < ApplicationController
     
     def authenticate
-        @user = User.find_by(user_email)
-        render json: @user
+        # binding.pry
+        @user = User.find_by(email: user_params[:email])
+        
+        
+        if @user && @user.authenticate(user_params[:password])
+            # ENCODE JWT TOKEN WITH USER_ID AND SECRET KEY
+            token = JWT.encode({user_id: @user.id}, "t3sts3cr3t")
+            render json: {
+                token: token
+            }
+        else
+            render json: {
+                error: "Invalid creditentials!"
+            }
+        end
     end
 
 
     private
 
-    def user_email
-        params.require(:user).permit(:email)
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name)
     end
+    
+
+
 end
