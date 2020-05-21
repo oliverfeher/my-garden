65a4a3b9f25e031ca45a4f82dee945f45e32249f
oliverfeher <oliver92.feher@gmail.com>
2020-05-21 08:32:33 -0400

    Build controlled React Form
    - Set input values aligned with state
    - Build onChange functions to set state

--- mygarden-frontend/src/components/Welcome.jsx
+++ mygarden-frontend/src/components/Welcome.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
-const Welcome = () => {
-  return (
-    <div>
-      <h1>Logged in</h1>
-    </div>
-  );
+class Welcome extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+
+    handleOnChangeEmail = (event) =>
+    {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handleOnChangePassword = (event) =>
+    {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    render()
+    {
+        return (
+            <form>
+                <label>Email:</label>
+                <br/>
+                <input type="e-mail" value={this.state.email} onChange={this.handleOnChangeEmail}/>
+                <br/>
+                <label>Password</label>
+                <br/>
+                <input type="password" value={this.state.password} onChange={this.handleOnChangePassword}/>
+                <br />
+                <input type="submit"/>
+            </form>
+        )
+    }
 }
 
 export default Welcome;
