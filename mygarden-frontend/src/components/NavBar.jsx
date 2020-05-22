21e447a525cd77f57967f53329762f51f2ea6eaf
oliverfeher <oliver92.feher@gmail.com>
2020-05-22 12:37:07 -0400

    Finish building NavBar
    - Add project logo
    - Stlye navbar

--- mygarden-frontend/src/components/NavBar.jsx
+++ mygarden-frontend/src/components/NavBar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Logo from "../images/logo.png"
 class NavBar extends React.Component
 {
     render()
     {
         return (
             <nav>
-                <Link to={"/dashboard"} id="plant" className="">dashboard</Link>
-                <Link to={"/plant"} id="plant" className="">plant</Link>
-                <Link to={"/history"} id="plant" className="">history</Link>
-                <Link to={"/current"} id="plant" className="">current</Link>
-                <Link to={"/"} onClick={()=>{localStorage.removeItem("token")}} id="plant" className="">logout</Link>
+                <img src={Logo} id="nav-logo" />
+                <div id="nav-bar">
+                    <Link to={"/dashboard"} id="plant" className="">dashboard</Link>
+                    <Link to={"/plant"} id="plant" className="">plant</Link>
+                    <Link to={"/history"} id="plant" className="">history</Link>
+                    <Link to={"/current"} id="plant" className="">current</Link>
+                    <Link to={"/"} onClick={()=>{localStorage.removeItem("token")}} id="plant" className="">logout</Link>
+                </div>
             </nav>
         )
     }
