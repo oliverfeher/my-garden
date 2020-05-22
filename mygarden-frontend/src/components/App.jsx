692393dccd982b3f82874db95030d75f83ca471e
oliverfeher <oliver92.feher@gmail.com>
2020-05-22 18:09:40 -0400

    Cleanup App.jsx

--- mygarden-frontend/src/components/App.jsx
+++ mygarden-frontend/src/components/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import "../stylesheets/App.css";
-import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from './Dashboard';
 import Welcome from "./Welcome"
 import Signup from "./Signup";
-// import Login from './Login';
-// import Logout from './Logout';
 
 
 class App extends React.Component {
