2778bfeac7116cb1572c61445c7247713c5e2cc0
oliverfeher <oliver92.feher@gmail.com>
2020-05-22 12:30:11 -0400

    Style NavBar

--- mygarden-frontend/src/components/Dashboard.jsx
+++ mygarden-frontend/src/components/Dashboard.jsx
@@ -55,7 +55,7 @@ class Dashboard extends React.Component
       return (
         <div id="dashboard">
           <NavBar />
-          <h1>Hello {this.state.user.first_name}</h1>
+          <h1>Welcome back, {this.state.user.first_name}!</h1>
           {!this.state.coords ? <p>Your location is: Loading...</p> : <Weather location={this.state.coords}/>}
           <button onClick={()=>{localStorage.removeItem("token"); this.props.history.push("/")}}>logout</button>
         </div>
