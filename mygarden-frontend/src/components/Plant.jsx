9fb20c22cc6a293e6312693e7b6fa5eec1879d22
oliverfeher <oliver92.feher@gmail.com>
2020-05-23 12:53:16 -0400

    Build controlled form for plant

--- mygarden-frontend/src/components/Plant.jsx
+++ mygarden-frontend/src/components/Plant.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 import axios from "axios";
+import CurrentDate from "../helpers/CurrentDate"
 
 class Plant extends React.Component
 {
     constructor(props)
     {
         super(props);
+        this.state = {
+            name: "",
+            days: 0,
+            date: ""
+        }
 
     }
 
+    handleOnChangeName = (event) =>
+    {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnChangeDate = (event) =>
+    {
+        this.setState({
+            date: event.target.value
+        })
+    }
+
+    handleOnChangeDays = (event) =>
+    {
+        this.setState({
+            days: event.target.value
+        })
+    }
 
     render()
     {
@@ -19,7 +45,19 @@ class Plant extends React.Component
         }
         else
         {
-            return <div>Planting</div>
+            return (
+                <div id="planting-form-container">
+                    <form id="planting-form" onSubmit={this.handleOnSubmit}>
+                        <label>plant's name</label>
+                        <input type="text" value={this.state.name} onChange={this.handleOnChangeName}/>
+                        <label>planting date</label>
+                        <input type="date" value={this.state.date} onChange={this.handleOnChangeDate}/>
+                        <label>expected to harvest</label>
+                        <input type="number" value={this.state.days} onChange={this.handleOnChangeDays}/>
+                        <input type="submit"/>  
+                    </form>
+                </div>
+            )
         }
     }
 }
--- mygarden-frontend/src/helpers/CurrentDate.jsx
+++ mygarden-frontend/src/helpers/CurrentDate.jsx
@@ -3,7 +3,8 @@
 const getTodaysDate = () => {
     let date = new Date();
     const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
-    let currentDate = ` ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    let currentDate = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    console.log(currentDate);
     return currentDate;
 }
 export default getTodaysDate;
