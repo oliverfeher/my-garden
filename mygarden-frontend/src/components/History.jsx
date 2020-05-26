d84667b7d4c5c03c58b2495b565350ff13bb0eb6
oliverfeher <oliver92.feher@gmail.com>
2020-05-26 18:32:34 -0400

    Build basic History component

---
+++ mygarden-frontend/src/components/History.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from "axios";
+
+class History extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            plants = []
+        }
+    }
+
+    render()
+    {
+        return (
+            <div>History comp</div>
+        )
+    }
+}
+
+export default History;
